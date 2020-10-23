037061c4d9914a0457065528319ed75f37f8ac9b
xabig <xabig@DESKTOP-1VCILET>
2020-10-23 12:27:57 +0200

    Commit de creacion de la clase ConnectionPool.

---
+++ Reto1Server/src/comicSans/com/Reto1Server/Pool/ConnectionPool.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comicSans.com.Reto1Server.Pool;
+
+/**
+ *
+ * @author xabig
+ */
+public class ConnectionPool {
+    
+    private final String DB = "comicsansdb";
+    private final String URL = "jdbc:mysql://localhost:3306/comicsansdb";
+    private final String USER = "root";
+    private final String PASS = "abcd*1234";
+}
