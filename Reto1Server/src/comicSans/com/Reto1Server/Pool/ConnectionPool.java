aa0ec4a70e2976eb28ebd9e088a41503d0b17f27
xabig <xabig@DESKTOP-1VCILET>
2020-10-23 23:29:40 +0200

    Commit de creacion de la clase ConnectionPool.

--- Reto1Server/src/comicSans/com/Reto1Server/Pool/ConnectionPool.java
+++ Reto1Server/src/comicSans/com/Reto1Server/Pool/ConnectionPool.java
@@ -5,6 +5,11 @@
  */
 package comicSans.com.Reto1Server.Pool;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+
 /**
  *
  * @author xabig
@@ -15,4 +20,32 @@ public class ConnectionPool {
     private final String URL = "jdbc:mysql://localhost:3306/comicsansdb";
     private final String USER = "root";
     private final String PASS = "abcd*1234";
+    
+    private static ConnectionPool dataSource;
+    private BasicDataSource basicDataSource = null;
+    
+    private ConnectionPool(){
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        basicDataSource.setUsername(USER);
+        basicDataSource.setPassword(PASS);
+        basicDataSource.setUrl(URL);
+        
+        
+    }
+    
+    public static ConnectionPool getInstance(){
+        if(dataSource==null){
+            dataSource = new ConnectionPool();
+        }
+        return dataSource;
+    }
+    
+    public Connection getConnection() throws SQLException{
+        return this.basicDataSource.getConnection();
+    }
+    
+    public void closeConnection(Connection connection) throws SQLException{
+        connection.close();
+    }
 }
--- Reto1Server/src/comicSans/com/Reto1Server/Server.java
+++ Reto1Server/src/comicSans/com/Reto1Server/Server.java
@@ -5,6 +5,12 @@
  */
 package comicSans.com.Reto1Server;
 
+import comicSans.com.Reto1Server.Pool.ConnectionPool;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jonyv
@@ -15,7 +21,22 @@ public class Server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try{
+            Connection c = ConnectionPool.getInstance().getConnection();
+            if(c != null){
+                System.out.println("Conectado!");
+                ConnectionPool.getInstance().closeConnection(c);
+            } else {
+                System.out.println("No conectado!");
+            }
+                            
+        } catch(SQLException ex){
+            //Logger log = log.log(ex.getMessage());
+            System.out.println(ex.getMessage());
+        }
+        
+        
     }
     
 }
