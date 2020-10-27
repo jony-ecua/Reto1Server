69c1c7ca53037b432357064ed92d8be54daab53d
xabig <xabig@DESKTOP-1VCILET>
2020-10-27 20:56:10 +0100

    Commit del reto Server con el Server a medias.

--- Reto1Server/src/comicSans/com/Reto1Server/Server.java
+++ Reto1Server/src/comicSans/com/Reto1Server/Server.java
@@ -6,10 +6,19 @@
 package comicSans.com.Reto1Server;
 
 import comicSans.com.Reto1Server.Pool.ConnectionPool;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import message.MessageReto;
 
 /**
  *
@@ -21,22 +30,61 @@ public class Server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        ServerSocket servidor;
+        Socket sc;
+        final int PUERTO = 1300;
         
-        try{
+        //ObjectInputStream in;
+        //ObjectOutputStream out;
+        DataInputStream in;
+        DataOutputStream out;
+        
+        try {
+            servidor = new ServerSocket(PUERTO);
+            System.out.println("Servidor conectado!");
+
+            while (true) {
+                sc = servidor.accept();
+
+                System.out.println("Cliente conectado!");
+
+                //in = new ObjectInputStream(sc.getInputStream());
+                //out = new ObjectOutputStream(sc.getOutputStream());
+                //MessageReto mes = (MessageReto) in.readObject();
+                //System.out.println(mes.getUser().getFullName());//Para comrpbar que el mensaje llega
+
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+                
+                String mensaje = in.readUTF();
+                System.out.println(mensaje);
+                
+                sc.close();
+                System.out.println("Cliente desconectado!");
+
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        //} catch (ClassNotFoundException ex) {
+        //    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
             Connection c = ConnectionPool.getInstance().getConnection();
-            if(c != null){
+            if (c != null) {
                 System.out.println("Conectado!");
                 ConnectionPool.getInstance().closeConnection(c);
             } else {
                 System.out.println("No conectado!");
             }
-                            
-        } catch(SQLException ex){
+
+        } catch (SQLException ex) {
             //Logger log = log.log(ex.getMessage());
             System.out.println(ex.getMessage());
         }
-        
-        
+
     }
-    
+
 }
---
+++ Reto1Server/src/comicSans/worker/Worker.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comicSans.worker;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+import message.MessageReto;
+import message.Type;
+import user.User;
+
+/**
+ *
+ * @author Jonathan Viñan
+ */
+public class Worker extends Thread {
+
+    MessageReto mes = new MessageReto();
+
+    public Worker(MessageReto aux) {
+        this.start();
+        this.mes = aux;
+    }
+
+    public void run() {
+
+        switch (mes.getType()) {
+            case SIGN_IN:
+
+                break;
+            case SIGN_UP:
+
+                break;
+            case LOGOUT:
+
+                break;
+        }
+
+    }
+
+}
